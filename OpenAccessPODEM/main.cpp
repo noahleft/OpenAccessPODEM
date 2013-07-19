9803dccdb379d340951abd96632b4148a67bfb63
Shih Feng Lin <noahleft@gmail.com>
2013-07-19 11:09:20 +0800

    Initial Commit

---
+++ OpenAccessPODEM/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  OpenAccessPODEM
+//
+//  Created by lin shih-feng on 13/7/19.
+//  Copyright (c) 2013年 lin shih-feng. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
