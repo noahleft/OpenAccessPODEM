2b5e7ef4bdcef2679d09c8a4cde4a391db657987
Shih Feng Lin <noahleft@gmail.com>
2013-07-19 23:57:35 +0800

    initial commit for OA define "OA_LibraryPath.h" and "oaDesignDB.h"

---
+++ OpenAccessPODEM/OA_LibraryPath.h
@@ -0,0 +1,4 @@
+
+//#include "/newhome/design_tools/oa22.43/examples/oa/common/commonTechObserver.h"
+//#include "/newhome/design_tools/oa22.43/examples/oa/common/commonLibDefListObserver.h"
+//#include "/newhome/design_tools/oa22.43/examples/oa/common/commonFunctions.h"
---
+++ OpenAccessPODEM/oaDesignDB.h
@@ -0,0 +1,17 @@
+//
+//  oaDesignDB.h
+//  OpenAccessPODEM
+//
+//  Created by lin shih-feng on 13/7/19.
+//  Copyright (c) 2013年 lin shih-feng. All rights reserved.
+//
+
+#ifndef OpenAccessPODEM_oaDesignDB_h
+#define OpenAccessPODEM_oaDesignDB_h
+
+namespace oa {
+    class oaDesign;
+    class oaLib;
+}
+
+#endif
